9016956e49f3d88330d7004e09608a962c6d0c4b
Francisco Miguez <franmiguez64@gmail.com>
2025-11-06 20:43:42 -0500

    Popup de espera y etc

--- src/details/detail.js
+++ src/details/detail.js
@@ -6,11 +6,40 @@ const addProductToCart = (id) => {
     if (productToAdd) {
         const existingProductIndex = carrito.findIndex(prod => prod.id === id);
         if (existingProductIndex !== -1) {
-            alert(`${productToAdd.nombre} ya está en el carrito.`);
+            const prodcarrito = new ProductoCarritoHelper(`${productToAdd.nombre} ya está en el carrito`, false);
+            postAddToCarrito(prodcarrito);
         } else {
             carrito.push({ ...productToAdd, cantidad: 1 });
             localStorage.setItem('carrito', JSON.stringify(carrito));
-            alert(`${productToAdd.nombre} ha sido agregado al carrito.`);
+            const prodcarrito = new ProductoCarritoHelper(`${productToAdd.nombre} ha sido agregado al carrito`, true);
+            postAddToCarrito(prodcarrito);
         }
+    }
+}
+
+const postAddToCarrito = (helper) => {
+    if (helper.valid){
+        $("#popupExito").text(helper.description);
+        $("#popupExito").show();
+        $("#popupError").hide();
+    } else {
+        $("#popupError").text(helper.description);
+        $("#popupExito").hide();
+        $("#popupError").show();
+    }
+
+    setTimeout(() => {
+        $("#popupExito").hide();
+        $("#popupError").hide();
+    }, 2000);
+}
+
+class ProductoCarritoHelper {
+    description = "";
+    valid = false;
+
+    constructor(description, valid){
+        this.description = description;
+        this.valid = valid;
     }
 }
